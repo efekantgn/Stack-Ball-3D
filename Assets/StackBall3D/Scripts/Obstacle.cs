23a5862e4e6c5917a0ba9779b6feaa2f1893f204
efekantgn <efekantgn@gmail.com>
2024-04-23 21:04:55 +0300

    fix: objectPooling Removed

--- Assets/StackBall3D/Scripts/Obstacle.cs
+++ Assets/StackBall3D/Scripts/Obstacle.cs
@@ -1,32 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.Animations;
-using UnityEngine.Pool;
 
 public class Obstacle : MonoBehaviour
 {
-    private ObjectPool<Obstacle> _pool;
     public float moveSpeed = 10f;
-
-    private void Start()
-    {
-        GameManager.instance.OnGameEnded += () =>
-        {
-
-        };
-    }
-
 
     public void MoveUp()
     {
-        transform.DOMoveY(10, 100 / moveSpeed).SetEase(Ease.Linear).OnComplete(() => ReleaseOnMoveEnd());
-    }
-
-    private void ReleaseOnMoveEnd()
-    {
-        _pool.Release(this);
+        transform.DOMoveY(10, 100 / moveSpeed).SetEase(Ease.Linear);
     }
 
     public void SetColor(Color color)
@@ -34,9 +15,5 @@ public class Obstacle : MonoBehaviour
         GetComponent<Renderer>().material.color = color;
     }
 
-    public void SetPool(ObjectPool<Obstacle> pool)
-    {
-        _pool = pool;
-    }
 
 }
--- Assets/StackBall3D/Scripts/ObstacleSpawner.cs
+++ Assets/StackBall3D/Scripts/ObstacleSpawner.cs
@@ -8,34 +8,7 @@ public class ObstacleSpawner : MonoBehaviour
     [SerializeField] private Obstacle obstacle;
     private Color obstacleColor = new Color(0, 0, 0);
     private float colorChanger = 0;
-    private ObjectPool<Obstacle> _pool;
     private Coroutine spawner;
-
-    private void Awake()
-    {
-        _pool = new ObjectPool<Obstacle>(CreateBullet, OnTakeObstacleFromPool, OnReturnObstacleToPool, OnDestroyObstacle, true, 10, 1000);
-    }
-
-    private void OnDestroyObstacle(Obstacle obstacle)
-    {
-        Destroy(obstacle.gameObject);
-    }
-
-    public void OnReturnObstacleToPool(Obstacle obstacle)
-    {
-        obstacle.gameObject.SetActive(false);
-    }
-    private void OnTakeObstacleFromPool(Obstacle obstacle)
-    {
-        obstacle.gameObject.SetActive(true);
-    }
-
-    private Obstacle CreateBullet()
-    {
-        Obstacle obstacles = Instantiate(obstacle);
-        obstacles.SetPool(_pool);
-        return obstacles;
-    }
 
     void Start()
     {
@@ -60,13 +33,12 @@ public class ObstacleSpawner : MonoBehaviour
 
             foreach (var item in spawnPoints)
             {
-                Obstacle obstacle = _pool.Get();
+                Obstacle obstacle =
                 obstacle.transform.SetParent(item.transform);
                 obstacle.transform.position = item.transform.position;
                 obstacle.MoveUp();
                 obstacle.SetColor(obstacleColor);
             }
-            Debug.Log("a");
         }
     }
 }
